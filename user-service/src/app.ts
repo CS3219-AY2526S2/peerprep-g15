fed16568f1c148f2fd70870807d61e15a3afb1e3
Shane <30289507+Shantaa@users.noreply.github.com>
2026-02-20 18:32:32 +0800

    Set up simple fiiles
    Including connection to db, some middleware, server and app setup

---
+++ user-service/src/app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import { registerRoutes } from './routes';
+import { notFoundHandler } from './middleware/notFound.middleware';
+import { errorHandler } from './middleware/error.middleware';
+
+export function createApp() {
+    const app = express();
+
+    // Base middleware
+    app.use(express.json());
+
+    // Routes
+    registerRoutes(app);
+
+    // 404 + error handlers (always last)
+    app.use(notFoundHandler);
+    app.use(errorHandler);
+
+    return app;
+}
