bacc43c3f7d4f4649a015bc2ef5e0352a58550fe
KiatLun <louiskhoo368@gmail.com>
2026-03-22 23:56:06 +0800

    Setup react app and login page

---
+++ frontend/peerprep-g15/src/components/SubmitButton.tsx
@@ -0,0 +1,9 @@
+const SubmitButton = () => {
+    return (
+        <div>
+            <button className="btn btn-secondary">Sign In</button>
+        </div>
+    );
+};
+
+export default SubmitButton;
